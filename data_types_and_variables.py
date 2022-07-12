3ac4e61d814f90b5e069e8284c2b2130c9d9a8e4
Gaziz <gazizmakhanov89@gmail.com>
2022-07-12 14:52:00 -0500

    my python exercise

---
+++ data_types_and_variables.py
@@ -0,0 +1,50 @@
+from operator import truediv
+from pickle import TRUE
+
+
+little_mermaid=3
+brother_bear=5
+hercules=1
+
+payment=little_mermaid*3+brother_bear*3+hercules*3
+
+print(payment);
+
+google=400
+amazon=380
+facebook=350
+
+paycheck=facebook*10+google*6+amazon*4
+
+print(paycheck);
+
+the_class='not full'
+class_schedule='current_schedule'
+if the_class == 'not full' and class_schedule!='current_schedule':
+    enroll==true   
+
+
+the_buy=15
+expiration='not expired'
+premium_buyer=TRUE
+
+if the_buy>=2 and expiration=='not expired'and premium_buyer==TRUE:
+    product_offer=TRUE
+
+
+# Create a variable that holds a boolean value for each of the following conditions:
+
+# the password must be at least 5 characters
+# the username must be no more than 20 characters
+# the password must not be the same as the username
+# bonus neither the username or password can start or end with whitespace
+ 
+password_length=17
+username_length=18
+pasword!=username
+
+if password_length==17 and username_length==8 and pasword!=username:
+    valid_pasword==TRUE
+
+
+
